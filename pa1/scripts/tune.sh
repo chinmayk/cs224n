525fec180128e8c3482d2bb1194aac9978d062b9
Chinmay Kulkarni <chinmay@stanford.edu>
2012-10-11 16:57:32 -0700

    minor edits

--- pa1/scripts/tune.sh
+++ pa1/scripts/tune.sh
@@ -17,9 +17,9 @@ ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev.en
 ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.fr
 ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.en
 
-set HOME=~/cs224n/pa1-mt
-set MOSES=/afs/ir/class/cs224n/bin/mosesdecoder
-set GIZA=/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir
+export HOME=~/cs224n/pa1-mt
+export MOSES=/afs/ir/class/cs224n/bin/mosesdecoder
+export GIZA=/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir
 
 #Get alignments
 java -cp ~/cs224n/pa1/java/classes cs224n.assignments.WordAlignmentTester \
