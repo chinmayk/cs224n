b6b6f0637ec83052fa177ffdd1a45d0e530cc6ae
Chinmay Kulkarni <chinmay@stanford.edu>
2012-10-11 16:54:20 -0700

    added basic tune script

---
+++ pa1/scripts/tune.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/sh
+
+#Setup
+cd
+cd cs224n/pa1-mt
+ln -s /afs/ir/class/cs224n/pa1/data/mt/lm.bin
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev.fr
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev.en
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.fr
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.en
+
+cd
+cd cs224n/pa1-mt
+ln -s /afs/ir/class/cs224n/pa1/data/mt/lm.bin
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev.fr
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev.en
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.fr
+ln -s /afs/ir/class/cs224n/pa1/data/mt/mt-dev-test.en
+
+set HOME=~/cs224n/pa1-mt
+set MOSES=/afs/ir/class/cs224n/bin/mosesdecoder
+set GIZA=/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir
+
+#Get alignments
+java -cp ~/cs224n/pa1/java/classes cs224n.assignments.WordAlignmentTester \
+-dataPath ~/cs224n \
+-model cs224n.wordaligner.PMIModel -language pa1-mt \
+-outputAlignments ~/cs224n/pa1-mt/training/corpus.align \
+-trainSentences 200
+
+#Get default phrase table
+mkdir -p $HOME/train/model
+$MOSES/scripts/training/train-model.perl --max-phrase-length 6 \
+--external-bin-dir $GIZA --first-step 4 --last-step 9 \
+-root-dir $HOME/train -corpus $HOME/training/corpus -f f -e e \
+-alignment-file $HOME/training/corpus -alignment align \
+-lm 0:3:"$HOME"/lm.bin:8
