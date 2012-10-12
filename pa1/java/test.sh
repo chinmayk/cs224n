f7b17ff9dce6dbd2863963a88d3cf30d170f95c9
Chinmay Kulkarni <chinmay@corn24.stanford.edu>
2012-10-12 16:56:28 -0700

    changed the IBM script

---
+++ pa1/java/test.sh
@@ -0,0 +1,4 @@
+HOME=~/cs224n/pa1-mt
+
+mkdir -p $HOME/train/model
+
--- pa1/scripts/tune.sh
+++ pa1/scripts/tune.sh
@@ -21,6 +21,10 @@ HOME=~/cs224n/pa1-mt
 MOSES=/afs/ir/class/cs224n/bin/mosesdecoder
 GIZA=/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir
 
+export HOME;
+export MOSES;
+export GIZA;
+
 # #Get alignments
 # java -cp ~/cs224n/pa1/java/classes cs224n.assignments.WordAlignmentTester \
 # -dataPath ~/cs224n \
@@ -29,12 +33,12 @@ GIZA=/afs/ir/class/cs224n/bin/giza-pp-read-only/external-bin-dir
 # -trainSentences 200
 
 # #Get default phrase table
-# mkdir -p $HOME/train/model
-# $MOSES/scripts/training/train-model.perl --max-phrase-length 6 \
-# --external-bin-dir $GIZA --first-step 4 --last-step 9 \
-# -root-dir $HOME/train -corpus $HOME/training/corpus -f f -e e \
-# -alignment-file $HOME/training/corpus -alignment align \
-# -lm 0:3:$HOME/lm.bin:8
+mkdir -p $HOME/train/model
+$MOSES/scripts/training/train-model.perl --max-phrase-length 6 \
+--external-bin-dir $GIZA --first-step 4 --last-step 9 \
+-root-dir $HOME/train -corpus $HOME/training/corpus -f f -e e \
+-alignment-file $HOME/training/corpus -alignment align \
+ -lm 0:3:$HOME/lm.bin:8
 
 # #Tune parameters
 # $MOSES/scripts/training/mert-moses.pl \
@@ -51,6 +55,7 @@ $MOSES/scripts/generic/multi-bleu.perl mt-dev-test.en < mt-dev-test.out > ~/cs22
 $MOSES/scripts/training/mert-moses.pl \
 --working-dir $HOME/tune \
 --decoder-flags="-distortion-limit 2" $HOME/mt-dev.fr $HOME/mt-dev.en \
+-nbest=10 \
 $MOSES/bin/moses $HOME/train/model/moses.ini --mertdir $MOSES/bin/
 
 $MOSES/scripts/generic/multi-bleu.perl mt-dev-test.en < mt-dev-test.out > ~/cs224n/pa1/results/distort2MERTPhrase6.txt
@@ -59,20 +64,20 @@ rm -rf $HOME/train/model/
 
 
 #Use a smaller phrase length
-mkdir -p $HOME/train/model
-$MOSES/scripts/training/train-model.perl --max-phrase-length 4 \
---external-bin-dir $GIZA --first-step 4 --last-step 9 \
--root-dir $HOME/train -corpus $HOME/training/corpus -f f -e e \
--alignment-file $HOME/training/corpus -alignment align \
--lm 0:3:$HOME/lm.bin:8
+#mkdir -p $HOME/train/model
+#$MOSES/scripts/training/train-model.perl --max-phrase-length 4 \
+#--external-bin-dir $GIZA --first-step 4 --last-step 9 \
+#-root-dir $HOME/train -corpus $HOME/training/corpus -f f -e e \
+#-alignment-file $HOME/training/corpus -alignment align \
+#-lm 0:3:$HOME/lm.bin:8
 
-$MOSES/scripts/training/mert-moses.pl \
---working-dir $HOME/tune \
---decoder-flags="-distortion-limit 4" $HOME/mt-dev.fr $HOME/mt-dev.en \
-$MOSES/bin/moses $HOME/train/model/moses.ini --mertdir $MOSES/bin/
+#$MOSES/scripts/training/mert-moses.pl \
+#--working-dir $HOME/tune \
+#--decoder-flags="-distortion-limit 4" $HOME/mt-dev.fr $HOME/mt-dev.en \
+#$MOSES/bin/moses $HOME/train/model/moses.ini --mertdir $MOSES/bin/
 
-cat $HOME/mt-dev-test.fr | $MOSES/scripts/moses-parallel.pl -decoder $MOSES/bin/moses -du \
--f $HOME/tune/moses.ini -jobs 8 > mt-dev-test.out
+#cat $HOME/mt-dev-test.fr | $MOSES/scripts/moses-parallel.pl -decoder $MOSES/bin/moses -du \
+#-f $HOME/tune/moses.ini -jobs 8 > mt-dev-test.out
 
-$MOSES/scripts/generic/multi-bleu.perl mt-dev-test.en < mt-dev-test.out > ~/cs224n/pa1/results/distort4MERTPhrase6.txt
+#$MOSES/scripts/generic/multi-bleu.perl mt-dev-test.en < mt-dev-test.out > ~/cs224n/pa1/results/distort4MERTPhrase6.txt
 
